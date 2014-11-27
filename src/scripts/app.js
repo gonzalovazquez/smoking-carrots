8dcc3389dd257e6a49774e9eaf0ad50dceaa7cc7
gonzalovazquez <gonzalo.segura1@gmail.com>
2014-11-27 17:09:55 -0500

    fixed dependecy on task

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,10 +2,10 @@ var game = new Phaser.Game(568, 320, Phaser.CANVAS, 'smoking-carrots',
 	{ preload: preload, create: create, update: update, render: render});
 
 var bullets;
-var fireButton;
 var bulletTime = 0;
 var hero;
-var cursor;
+var currentSpeed = 0;
+var cursors;
 var zombies;
 var score = 0;
 var numberOfLives = 3;
@@ -26,7 +26,6 @@ function preload() {
 
 function create() {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
-
 	game.world.setBounds(0, 0, 800, 600);
 	game.add.sprite(0,0,'background');
 
@@ -39,7 +38,8 @@ function create() {
 	game.physics.enable(hero, Phaser.Physics.ARCADE);
 	hero.enableBody = true;
 	hero.body.collideWorldBounds = true;
-	hero.body.immovable = false;
+	hero.body.immovable = true;
+	hero.bringToTop();
 
 	hero.anchor.setTo(0.5, 0.5);
 
@@ -68,36 +68,13 @@ function create() {
 	//Lives
 	livesText = game.add.text(0, 550, ' lives: ' + lives, {font: '30px Helvetica', fill: '#fff'});
 
-	//Joystick control
-	GameController.init({
-		left: {
-			type: 'joystick',
-			joystick: {
-				touchMove: function(details) {
-					game.input.joystickLeft = details;
-				},
-				touchEnd: function() {
-					game.input.joystickLeft = null;
-				}
-			}
-		},
-		right: { 
-			type: 'joystick',
-			joystick: {
-				touchMove: function(details) {
-					game.input.joystickRight = details;
-				},
-				touchEnd: function() {
-					game.input.joystickRight = null;
-				}
-			}
-		}
-	});
-
 	// Camera control
 	game.camera.follow(hero);
 	game.camera.deadzone = new Phaser.Rectangle(150, 150, 150, 150);
 	game.camera.focusOnXY(0, 0);
+
+	//Our cursor
+	cursors = game.input.keyboard.createCursorKeys();
 }
 
 function createZombies(override) {
@@ -113,21 +90,44 @@ function createZombies(override) {
 }
 
 function update() {
-	//Evil bunnies cannot move hero
-	hero.body.immovable = true;
-
 	//  Run collision to kill enemies from hero's gun
 	game.physics.arcade.collide(bullets, zombies, collisionHandler, null, this);
 	game.physics.arcade.collide(hero, zombies, enemyHitsPlayer, null, this);
 
-	if (game.input.joystickLeft) {
-		moveHero(game.input.joystickLeft.dx * 2, game.input.joystickLeft.dy * 2);
-	} else if(game.input.joystickRight) {
-		rotateHero(game.input.joystickRight.normalizedX * 1000, game.input.joystickRight.normalizedY * 1000);
-		fireBullet(game.input.joystickRight.normalizedX * 200, game.input.joystickRight.normalizedY * 360);
+	//Hero Movement
+	if (cursors.left.isDown)
+	{
+		hero.angle -= 4;
+	}
+	else if (cursors.right.isDown)
+	{
+		hero.angle += 4;
 	}
 
-	zombies.angle = game.physics.arcade.angleBetween(zombies, hero);
+	if (cursors.up.isDown)
+	{
+		//  The speed we'll travel at
+		currentSpeed = 100;
+	}
+	else
+	{
+		if (currentSpeed > 0)
+		{
+			currentSpeed -= 4;
+		}
+	}
+
+	if (currentSpeed > 0)
+	{
+		game.physics.arcade.velocityFromRotation(hero.rotation, currentSpeed, hero.body.velocity);
+	}
+
+	//hero.rotation = game.physics.arcade.angleToPointer(hero);
+
+	//Hero shooting
+	if (Phaser.Keyboard.SPACEBAR.isDown) {
+		fireBullet();
+	}
 }
 
 function collisionHandler(bullet, evilBunny) {
@@ -157,7 +157,7 @@ function enemyHitsPlayer(hero, evilBunny) {
 	}
 }
 
-function fireBullet(x, y) {
+function fireBullet() {
 	//  To avoid them being allowed to fire too fast we set a time limit
 	if (game.time.now > bulletTime)
 	{
@@ -167,9 +167,8 @@ function fireBullet(x, y) {
 		if (bullet)
 		{
 			bullet.reset(hero.x - 7, hero.y - 26);
-			bullet.body.velocity.y = y * -1;
-			bullet.body.velocity.x = x;
 			bulletTime = game.time.now + 200;
+			bullet.rotation = game.physics.arcade.moveToPointer(bullet, 1000, game.input.activePointer, 500);
 		}
 	}
 
@@ -193,12 +192,3 @@ function render() {
  * HELPER FUNCTIONS
  *
 */
-
-function moveHero(x, y) {
-	hero.body.velocity.x = x;
-	hero.body.velocity.y = y * -1;
-}
-
-function rotateHero(x, y) {
-	hero.angle = (x + y);
-}
